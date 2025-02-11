bb109b6904a5ecd05b70b21f86b715cd506ccc74
unknown <cyprusakanni@gmail.com>
2025-02-11 19:59:15 +0100

    clean up

--- src/app/components/nav/index.tsx
+++ src/app/components/nav/index.tsx
@@ -1,54 +1,4 @@
-"use client";
-
-import styled from "styled-components";
 import Image from "next/image";
-
-const StyledParagraph = styled.p`
-  padding: 0.5rem;
-  cursor: pointer;
-  position: relative;
-  font-weight: 500;
-  color: inherit;
-
-  &:hover {
-    color: #eaeaea;
-  }
-
-  &::before {
-    content: "";
-    position: absolute;
-    bottom: -7.5px;
-    left: 0;
-    width: 0;
-    height: 2px;
-    background-color: #126cf8;
-    transition: width 0.3s ease-in-out;
-  }
-
-  &:hover::before {
-    width: 100%;
-  }
-`;
-
-const LogoWrapper = styled.div`
-  position: relative;
-  display: inline-block;
-  padding: 5px;
-  cursor: pointer;
-
-  &:hover::after {
-    content: "";
-    position: absolute;
-    bottom: 0px;
-    right: -7px;
-    width: 8px;
-    height: 8px;
-    background-color: #126cf8;
-    border-radius: 50%;
-
-    transition-delay: 0.5s ease-in-out;
-  }
-`;
 
 const navItems = [
   { id: "home", label: "Home" },
@@ -60,12 +10,18 @@ const navItems = [
 export const NavBar = () => {
   return (
     <div className="flex items-center  py-2 px-5 bg-[#080808] text-[#eaeaea] hover:text-[#939393] transition-colors duration-300 border-b-[0.5px] border-gray-500 sticky top-0 z-10">
-      <LogoWrapper>
+      <div className="relative inline-block p-1 cursor-pointer after:content-[''] after:absolute after:bottom-0 after:right-[-7px] after:w-0 after:h-0 after:bg-[#126cf8] after:rounded-full after:transition-all after:duration-500 hover:after:w-2 hover:after:h-2">
         <Image src="/Images/logo.png" alt="Logo" width={70} height={50} />
-      </LogoWrapper>
+      </div>
+
       <div className="flex flex-row justify-center items-center gap-10 m-auto">
         {navItems.map((navItem, index) => (
-          <StyledParagraph key={index}>{navItem.label}</StyledParagraph>
+          <p
+            key={index}
+            className="relative font-medium cursor-pointer p-2 transition-colors hover:text-[#eaeaea] after:content-[''] after:absolute after:bottom-[-7.5px] after:left-0 after:w-0 after:h-[2px] after:bg-[#126cf8] after:transition-[width] after:duration-300 hover:after:w-full"
+          >
+            {navItem.label}
+          </p>
         ))}
       </div>
     </div>
