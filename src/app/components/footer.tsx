5c7f12e59592178782281f5912f2a24afb540e4f
unknown <cyprusakanni@gmail.com>
2025-09-06 12:06:38 +0100

    more clean ups

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@
 
 import { TbBrandLinkedinFilled } from "react-icons/tb";
 import { FaGithub } from "react-icons/fa";
-import {  useState } from "react";
+import { useState } from "react";
 const socials = [
   {
     id: "LinkedIn",
@@ -17,9 +17,8 @@ const socials = [
 ];
 
 const FooterModule = () => {
-
   return (
-    <div className="my-10 text-[#7f828b] border-t-2 border-[#6d7078]">
+    <div className="my-10 text-[#9c9ea4] border-t-2 border-[#6d7078]">
       <div className=" flex m-auto max-w-7xl flex-col md:flex-row items-center md:justify-between justify-center mt-10 px-10 gap-y-5 md:gap-y-0">
         <p>Olamilekan Akanni &copy; 2025</p>
         <span className="flex gap-4 item-center">
@@ -30,8 +29,7 @@ const FooterModule = () => {
               target="_blank"
               rel="noopener noreferrer"
               aria-label={`Olamilekan's ${social.id}`}
-              className="transition-colors duration-300 hover:text-[#6e06f2] text-[#7f828b]"
-
+              className="transition-colors duration-300 hover:text-[#6e06f2] text-[#9c9ea4]"
             >
               {<social.icon size={25} />}
             </a>
