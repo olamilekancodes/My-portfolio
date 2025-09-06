cbdff9e306adf8de8ef1d1c78088a52d129babf5
unknown <cyprusakanni@gmail.com>
2025-09-06 14:00:18 +0100

    clean up

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,8 +2,15 @@
 
 import { TbBrandLinkedinFilled } from "react-icons/tb";
 import { FaGithub } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { Paragraph } from "../shared/Typography";
 
 const socials = [
+  {
+    id: "Mail",
+    icon: IoMdMail,
+    link: "mailto:cyprusakanni@gmail.com",
+  },
   {
     id: "LinkedIn",
     icon: TbBrandLinkedinFilled,
@@ -18,22 +25,27 @@ const socials = [
 
 const FooterModule = () => {
   return (
-    <div className="my-10 text-[#9c9ea4] border-t-2 border-[#6d7078]">
-      <div className=" flex m-auto max-w-7xl flex-col md:flex-row items-center md:justify-between justify-center mt-10 px-10 gap-y-5 md:gap-y-0">
-        <p>Olamilekan Akanni &copy; 2025</p>
+    <div className="  my-5 md:my-7 lg:my-10 text-[#9c9ea4] border-t-2 border-[#6d7078]">
+      <div className=" flex m-auto max-w-7xl flex-col md:flex-row items-center md:justify-between justify-center mt-3 md:mt-5 lg:mt-10 px-10 gap-y-3 md:gap-y-0">
+        <Paragraph>Olamilekan Akanni &copy; 2025</Paragraph>
         <span className="flex gap-4 item-center">
-          {socials.map((social) => (
-            <a
-              key={social.id}
-              href={social.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={`Olamilekan's ${social.id}`}
-              className="transition-colors duration-300 hover:text-[#6e06f2] text-[#9c9ea4]"
-            >
-              {<social.icon size={25} />}
-            </a>
-          ))}
+          {socials.map((social) => {
+            const isMail = social.id === "Mail";
+            return (
+              <a
+                key={social.id}
+                href={social.link}
+                {...(!isMail && {
+                  target: "_blank",
+                  rel: "noopener noreferrer",
+                })}
+                aria-label={`Olamilekan's ${social.id}`}
+                className="transition-colors duration-300 hover:text-[#6e06f2] text-[#9c9ea4]"
+              >
+                <social.icon className="w-[15px] h-[15px] md:w-[20px] md:h-[20px] lg:w-[25px] lg:h-[25px]" />
+              </a>
+            );
+          })}
         </span>
       </div>
     </div>
